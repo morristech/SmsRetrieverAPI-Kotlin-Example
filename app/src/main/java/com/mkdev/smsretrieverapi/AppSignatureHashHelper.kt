c717faa6eb4b5b122bed1a937e0ffe27f3287d35
mkdev <Mk2580@@>
2021-08-21 14:31:52 +0430

    Added AppSignatureHashHelper and SMSReceiver

---
+++ app/src/main/java/com/mkdev/smsretrieverapi/AppSignatureHashHelper.kt
@@ -0,0 +1,70 @@
+package com.mkdev.smsretrieverapi
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.pm.PackageManager
+import android.util.Base64
+import android.util.Log
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+
+class AppSignatureHashHelper(context: Context?) : ContextWrapper(context) {
+
+    /**
+     * Get all the app signatures for the current package
+     */
+    val appSignatures: ArrayList<String>
+        @SuppressLint("PackageManagerGetSignatures")
+        get() {
+            val appSignaturesHashes = ArrayList<String>()
+            try {
+                // Get all package details
+                val packageName = packageName
+                val packageManager = packageManager
+                val signatures = packageManager.getPackageInfo(
+                    packageName,
+                    PackageManager.GET_SIGNATURES
+                ).signatures
+
+                for (signature in signatures) {
+                    val hash = hash(packageName, signature.toCharsString())
+                    if (hash != null) {
+                        appSignaturesHashes.add(String.format("%s", hash))
+                    }
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "Package not found", e)
+            }
+            return appSignaturesHashes
+        }
+
+    companion object {
+        val TAG = AppSignatureHashHelper::class.java.simpleName
+        private const val HASH_TYPE = "SHA-256"
+        const val NUM_HASHED_BYTES = 9
+        const val NUM_BASE64_CHAR = 11
+
+        private fun hash(packageName: String, signature: String): String? {
+            val appInfo = "$packageName $signature"
+            try {
+                val messageDigest = MessageDigest.getInstance(HASH_TYPE)
+                messageDigest.update(appInfo.toByteArray(StandardCharsets.UTF_8))
+                var hashSignature = messageDigest.digest()
+
+                // truncated into NUM_HASHED_BYTES
+                hashSignature = Arrays.copyOfRange(hashSignature, 0, NUM_HASHED_BYTES)
+                // encode into Base64
+                var base64Hash =
+                    Base64.encodeToString(hashSignature, Base64.NO_PADDING or Base64.NO_WRAP)
+                base64Hash = base64Hash.substring(0, NUM_BASE64_CHAR)
+                return base64Hash
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e(TAG, "No Such Algorithm Exception", e)
+            }
+            return null
+        }
+    }
+}
--- app/src/main/java/com/mkdev/smsretrieverapi/MainActivity.kt
+++ app/src/main/java/com/mkdev/smsretrieverapi/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.mkdev.smsretrieverapi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.phone.SmsRetriever
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SMSReceiver.OTPReceiveListener {
+
+    private val TAG = MainActivity::class.java.simpleName
+    private var smsReceiver: SMSReceiver? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val appSignatureHashHelper = AppSignatureHashHelper(this)
+
+        // This code requires one time to get Hash keys do comment and share key
+        Log.i(TAG, "HashKey: " + appSignatureHashHelper.appSignatures[0])
+
+        startSMSListener()
+    }
+
+    /**
+     * Starts SmsRetriever, which waits for ONE matching SMS message until timeout
+     * (5 minutes). The matching SMS message will be sent via a Broadcast Intent with
+     * action SmsRetriever#SMS_RETRIEVED_ACTION.
+     */
+    private fun startSMSListener() {
+        try {
+            smsReceiver = SMSReceiver()
+            smsReceiver?.setOTPListener(this)
+
+            val intentFilter = IntentFilter()
+            intentFilter.addAction(SmsRetriever.SMS_RETRIEVED_ACTION)
+            registerReceiver(smsReceiver, intentFilter)
+
+            val client = SmsRetriever.getClient(this)
+            val task = client.startSmsRetriever()
+            task.addOnSuccessListener {
+                // API successfully started
+            }
+            task.addOnFailureListener {
+                // Fail to start API
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onOTPReceived(otp: String?) {
+        showToast("OTP Received: $otp");
+
+        if (smsReceiver != null) {
+            unregisterReceiver(smsReceiver);
+            smsReceiver = null;
+        }
+    }
+
+    override fun onOTPTimeOut() {
+        showToast("OTP Time out")
+    }
+
+    override fun onOTPReceivedError(error: String?) {
+        showToast(error ?: "ERROR")
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (smsReceiver != null) {
+            unregisterReceiver(smsReceiver)
+        }
     }
 }
